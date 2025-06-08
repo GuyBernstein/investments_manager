e46f14f198c3a29af664d8c0a8d70e43409b95f2
GuyBernstein <guyu669@gmail.com>
2025-06-08 11:47:51 +0300

    initial commit

---
+++ src/main/java/com/example/investments_manager/InvestmentsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.investments_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestmentsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestmentsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/investments_manager/InvestmentsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.investments_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
